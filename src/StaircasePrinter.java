eb2876b9b7f1db6fb35e2615c3d49bb74369a8db
matthewpgaffney <mgaffney81@yahoo.com>
2014-02-17 21:28:03 -0600

    Initial commit

---
+++ src/StaircasePrinter.java
@@ -0,0 +1,18 @@
+/**
+ * Created by matthewgaffney on 2/17/14.
+ */
+public class StaircasePrinter
+{
+    public static void main(String [] args)
+    {
+        System.out.println("            +---+");
+        System.out.println("            |   |");
+        System.out.println("        +---+---+");
+        System.out.println("        |   |   |");
+        System.out.println("    +---+---+---+");
+        System.out.println("    |   |   |   |");
+        System.out.println("+---+---+---+---+");
+        System.out.println("|   |   |   |   |");
+        System.out.println("+---+---+---+---+");
+    }
+}
